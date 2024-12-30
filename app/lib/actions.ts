70ea691fabc7b234414e7e30fcb2423e37c085ac
ShahoudGradus <119983324+ShahoudGradus@users.noreply.github.com>
2024-12-30 17:47:47 +0300

    Resolving error at build

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -117,16 +117,14 @@ export async function updateInvoice(
 
 //delete Invoice action
 export async function deleteInvoice(id: string) {
-  //throw new Error("Failed to Delete Invoice.");
   try {
     await sql`
       DELETE FROM  invoices WHERE id = ${id};
     `;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
   } catch (error) {
     console.log(error);
-    return { message: "Database Error, Failed to Delete Invoice." };
+    //return { message: "Database Error, Failed to Delete Invoice." };
   }
 }
 
